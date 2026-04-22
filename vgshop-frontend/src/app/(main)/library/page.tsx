4d70f000e00cb57a369e2e2ee66544e1c30f6f13
MatVecchi22 <mat.vecchi@gmail.com>
2026-04-22 16:54:24 +0200

    aggiunta pagina library

--- vgshop-frontend/src/app/(main)/library/page.tsx
+++ vgshop-frontend/src/app/(main)/library/page.tsx
@@ -1,5 +1,31 @@
+import { ArrowUpRightIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+
 export default function Library() {
-    return <div>
-        <h1>I tuoi giochi</h1>
+  return (
+    <div className="flex-1">
+      <h2 className="uppercase text-4xl font-bold">I tuoi giochi</h2>
+      <Empty>
+        <EmptyHeader>
+          <EmptyTitle>Non hai nessun gioco nella tua libreria</EmptyTitle>
+          <EmptyDescription>
+            Acquista un gioco per vederlo qui. Visita il nostro catalogo per
+            scoprire i giochi disponibili.
+          </EmptyDescription>
+        </EmptyHeader>
+        <EmptyContent className="flex-row justify-center gap-2">
+          <Button>Create Project</Button>
+        </EmptyContent>
+      </Empty>
     </div>
+  );
 }
